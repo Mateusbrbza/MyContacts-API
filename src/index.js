d50d80aa262f68889270adf8eeedda8648f56aac
mateusbrbza <mateus.c.barboza@gmail.com>
2023-06-09 18:43:47 -0300

    backend finalizado, iniciando o repo do front

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(8080, () => console.log('listening on port http://172.23.119.108:8080'));
