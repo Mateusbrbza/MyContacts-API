c9e9d0369633415a798e40168565939a6a882d44
mateusbrbza <mateus.c.barboza@gmail.com>
2023-06-20 09:27:26 -0300

    refatorando para iniciar integracao com fe

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const express = require('express');
 require('express-async-errors');
 
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(8080, () => console.log('listening on port http://172.23.119.108:8080'));
+app.listen(4000, () => console.log('listening on port http://localhost:4000'));
